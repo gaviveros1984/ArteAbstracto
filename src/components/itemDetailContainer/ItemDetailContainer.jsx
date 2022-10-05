ac82911f34947aef411cad9ac306bc8323380c1d
Gabriel Viveros <gabrielviveros79@gmail.com>
2022-10-04 22:33:40 -0300

    actual

--- src/components/itemDetailContainer/ItemDetailContainer.jsx
+++ src/components/itemDetailContainer/ItemDetailContainer.jsx
@@ -3,6 +3,7 @@ import  {ItemDetail}  from './itemDetail/ItemDetail'
 import  {useParams}  from 'react-router-dom'
 import { doc, getDoc } from 'firebase/firestore';
 import db from '../../service/firebase';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 
 export const ItemDetailContainer = () => {
 
@@ -12,11 +13,11 @@ export const ItemDetailContainer = () => {
 
   const getSelected = async (idItem) => {
     try {
-        const document = doc(db, "Items", idItem)
-        const response = await getDoc(document)
-        const result = { id: response.id, ...response.data() }
-        console.log(result)
-        setSelectedItem(result)
+      const document = query(collection (db, "Items"), where ("id", "==",Number(idItem)))
+      const col = await getDocs(document)
+      console.log("col.docs", col.docs)
+      const result = col.docs.map((doc) => doc = {id:doc.id,...doc.data()})
+      setSelectedItem(...result)
     } catch (error) {
         console.log(error)
     }
--- src/components/itemDetailContainer/itemDetail/ItemDetail.jsx
+++ src/components/itemDetailContainer/itemDetail/ItemDetail.jsx
@@ -4,6 +4,7 @@ import Card from 'react-bootstrap/Card';
 
 export const ItemDetail = ({item}) => {
 
+  console.log({item})
   return (
     <>
     <article>
